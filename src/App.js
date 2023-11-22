1a486a6584d1d8408d97374398d4ad53a8ca192c
CL <ongchangle03@gmail.com>
2023-11-23 01:49:23 +0800

    Using array.map to loop through array

--- src/App.js
+++ src/App.js
@@ -6,25 +6,17 @@ import './App.css';
 
 function App() {
 
-  const age = 19;
-  const isGreen = false;
-  
+  const names = ['Ali', 'Sam', 'John', 'Lucas'];
 
 
   return (
     <div className="App"> 
 
-      {/* Using Tenary Operator */}
-      {age >= 18 ? <h1>Over Age</h1> : <h1>Under Age</h1>}
-      
-      {/* Using style */}
-      <h1 style={{color: isGreen? "green" : "red"}}> This has color </h1>
-
-
-      {/* Using logical && for if statement without else branch */}
-      {/* If the green is true, show the button, else do nothing, really useful*/}
-      {isGreen && <button>This is a button</button>}
-
+      {/* Using array.map to loop through array */}
+      {names.map((name, key) => {
+        return <h1 key={key}> {name} </h1>;
+      })}
+    
 
     </div>
   );
