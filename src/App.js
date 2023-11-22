e2f13e9c190edb5f49c2d5c3c6c6ae5ea01f93d0
CL <ongchangle03@gmail.com>
2023-11-23 01:34:23 +0800

    Using tenary operator and styles together

--- src/App.js
+++ src/App.js
@@ -7,13 +7,21 @@ import './App.css';
 function App() {
 
   const age = 19;
+  const isGreen = false;
   
 
 
   return (
     <div className="App"> 
-      {/** Using Tenary Operator */}
+
+      {/* Using Tenary Operator */}
       {age >= 18 ? <h1>Over Age</h1> : <h1>Under Age</h1>}
+      
+      {/* Using style */}
+      <h1 style={{color: isGreen? "green" : "red"}}> This has color </h1>
+
+
+
     </div>
   );
 }
