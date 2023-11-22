d127bcba650bc2362d4df649960c03d6de043993
CL <ongchangle03@gmail.com>
2023-11-23 00:53:40 +0800

    Create first simple component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">Hello World</div>
+    <div className="App"> 
+      <User/>
+    </div>
   );
 }
+
+// component
+const User = () => {
+  return (
+    <div>
+      <h1>cl</h1>
+      <h2>21</h2>
+      <h2>cl@gmai.com</h2>
+    </div>
+  );
+}
+
 
 export default App;
