0185372b96063edeea506d9ddc25a8a154d40e27
CL <ongchangle03@gmail.com>
2023-11-23 01:04:10 +0800

    Using props to pass in data so we can reduce the code

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ function App() {
 
   return (
     <div className="App"> 
-      <User/>
+      {/* This is a JSX comment */}
+      {/* So using props we can pass in different data for creating same compoenent but with different value/data */}
+      <User name="cl" age={20} email="cl@gmail.com" />
+      <User name="second" age={21} email="second@gmail.com" />
     </div>
   );
 }
 
 // component
-const User = () => {
+// props is like data we can pass in into this components
+const User = (props) => {
   return (
     <div>
-      <h1>cl</h1>
-      <h2>21</h2>
-      <h2>cl@gmai.com</h2>
+      <h1>{props.name}</h1>
+      <h2>{props.age}</h2>
+      <h2>{props.email}</h2>
     </div>
   );
 }
