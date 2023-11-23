845c93c7b3d11873c10e0cd2dda71ba17cda77d2
CL <ongchangle03@gmail.com>
2023-11-23 12:05:20 +0800

    Using onChange in input with useState

--- src/App.js
+++ src/App.js
@@ -6,24 +6,17 @@ import { useState } from 'react';
 
 function App() {
 
-  // useState
-  const [age, setAge] = useState(0);      // 0 is initial state, can pass in any value / pure function
+  const [inputValue, setInputValue] = useState('');
 
-  function increaseAge() {
-    setAge(age + 1)           // setAge will set the age, it can pass in any value/function
+  // using onChange in input with useState
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
   }
-
 
   return (
     <div className="App"> 
-
-      {age}
-
-      <button onClick={increaseAge}> 
-        Increase Age 
-      </button>
-
-
+      <input type="text" onChange={handleInputChange} />
+      {inputValue}
     </div>
   );
 }
