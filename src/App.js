190f8e03d446dc3ba9dc66bd688d3b5b91ad36d9
CL <ongchangle03@gmail.com>
2023-11-23 11:58:13 +0800

    Discover useState

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import './App.css';
-import {Planet} from './Planet'
+import { useState } from 'react';
 
 
 
 
 function App() {
 
-  function handleClick() {
-    alert('You clicked me');
+  // useState
+  const [age, setAge] = useState(0);      // 0 is initial state, can pass in any value / pure function
+
+  function increaseAge() {
+    setAge(age + 1)           // setAge will set the age, it can pass in any value/function
   }
 
 
   return (
     <div className="App"> 
 
-      {/* button onClick  */}
-      <button onClick={handleClick}>
-        Click me
+      {age}
+
+      <button onClick={increaseAge}> 
+        Increase Age 
       </button>
 
 
