1ae3158d49e64fad6dffcd56e57312c2aca2edee
CL <ongchangle03@gmail.com>
2023-11-24 18:35:27 +0800

    Using Link to navigate diffetent pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { Home } from "./pages/Home";
 import { Menu } from "./pages/Menu";
 import { Contact } from "./pages/Contact";
@@ -11,6 +11,12 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <div>
+          <Link to="/"> Home </Link>
+          <Link to="/Menu"> Menu </Link>
+          <Link to="/Contact"> Contact </Link>
+        </div>
+
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/menu" element={<Menu />}/>
