18ef43bfa2b15545c425b6c4f69f45ecf48d5428
CL <ongchangle03@gmail.com>
2023-11-24 17:52:22 +0800

    fetch data with object and initiaze with null

--- src/App.js
+++ src/App.js
@@ -7,24 +7,24 @@ import {useEffect, useState} from 'react';
 
 function App() {
 
-  const [catFact, setCatFact] = useState("");
 
-  const fetchCatFact = () => {
-    Axios.get("https://catfact.ninja/fact")
-    .then(res => setCatFact(res.data.fact))
-    .catch(e => console.log(e))
+  const [name, setName] = useState('');
+  const [predictedAge, setPredictedAge] = useState(null);
+
+  const fetchData = () => {
+    Axios
+      .get(`https://api.agify.io/?name=${name}`)
+      .then(res => setPredictedAge(res.data))
   }
-
-  // fetch data in useEffect when mount (so only fetch once when mount)
-  useEffect(() => {
-    fetchCatFact();
-  }, []);
-
 
   return (
     <div className="App">
-      <button onClick={fetchCatFact}> Generate Cat Fact </button>
-      <p>{catFact}</p>
+      <input placeholder='Your name' onChange={event => setName(event.target.value)} />
+      <button onClick={fetchData}> Predict Age </button>
+
+      <h1> Name: {predictedAge?.name}</h1>
+      <h1> Predicted Age: {predictedAge?.age}</h1>
+      <h1> Count: {predictedAge?.count}</h1>
     </div>
   );
 }
