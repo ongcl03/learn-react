4a9c7af75061021a72234beaed65700782f215a3
CL <ongchangle03@gmail.com>
2023-11-24 17:37:33 +0800

    Fetch data in useEffect

--- src/App.js
+++ src/App.js
@@ -1,80 +1,30 @@
 import './App.css';
-import { useState, useEffect  } from 'react';
-import { Task } from './Task.js'
+import Axios from 'axios';
+import {useEffect, useState} from 'react';
 
 
 
 
 function App() {
 
-  const [todoList, setTodoList] = useState([]);
-  const [newTask, setNewTask] = useState('');
+  const [catFact, setCatFact] = useState("");
 
+  const fetchCatFact = () => {
+    Axios.get("https://catfact.ninja/fact")
+    .then(res => setCatFact(res.data.fact))
+    .catch(e => console.log(e))
+  }
 
+  // fetch data in useEffect when mount (so only fetch once when mount)
   useEffect(() => {
-    console.log("Component mounted");
-
-    return () => {
-      console.log("Component Unmounted");
-    }
-  }, [])
-
-
-
-  function handleChange(event) {
-    setNewTask(event.target.value)
-  }
-
-  function addTask() {
-
-    // Now toDoList is an array of object
-    const task = {
-      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
-      taskName: newTask,
-      isCompleted: false
-    };
-
-    setTodoList([...todoList, task]);
-  }
-
-  function deleteTask(taskId) {
-    // Now we will filter the id (better way when using id)
-    setTodoList(todoList.filter(task => task.id !== taskId));
-  }
-
-
-  function completeTask(taskId) {
-    setTodoList(
-      todoList.map((task) => {
-        if (task.id === taskId){
-          return {...task, isCompleted: true}
-        } else {
-          return task;
-        }
-      })
-    );
-  }
+    fetchCatFact();
+  }, []);
 
 
   return (
     <div className="App">
-
-      <div className="addTask">
-        <input onChange={handleChange} />
-        <button onClick={addTask}>Add Task</button>
-      </div>
-
-      <div className="list">
-
-        {/* Pass values into componenet include passing functions */}
-
-        {todoList.map((task) => {
-          return <Task taskName={task.taskName} id={task.id} deleteTask={deleteTask} completeTask={completeTask} isCompleted={task.isCompleted}/>
-        })}
-
-      </div>
-
-
+      <button onClick={fetchCatFact}> Generate Cat Fact </button>
+      <p>{catFact}</p>
     </div>
   );
 }
