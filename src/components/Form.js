5ae6270a7fd4409ee923b4a772c1110b4cdc9ee7
CL <ongchangle03@gmail.com>
2023-11-26 02:46:52 +0800

    Using yup to validate the form data

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,38 @@
 import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+// yup is to validate form format
+// react-hook-form is a hook for form and we will use useForm for handle form submission and register it for get the form data
 
 export const Form = () => {
-  const { register, handleSubmit } = useForm();
+  // Using yup to validate the form data format
+  const schema = yup.object().shape({
+    fullName: yup.string().required(),
+    email: yup.string().email().required(),
+    age: yup.number().positive().integer().min(18).required(),
+    password: yup.string().min(4).max(20).required(),
+    confirmPassword: yup
+      .string()
+      .oneOf([yup.ref("password"), null])
+      .required(),
+  });
 
+  // Using yupResolver to integrate react-hook-form and yup
+  const { register, handleSubmit } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  // This function will only run when the form data that fits the format
   const onSubmit = (data) => {
     console.log(data);
   };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      {/* need to register the data for the form */}
       <input type="text" placeholder="Full Name..." {...register("fullName")} />
       <input type="text" placeholder="Email..." {...register("email")} />
-      <input type="text" placeholder="Age..." {...register("age")} />
+      <input type="number" placeholder="Age..." {...register("age")} />
       <input
         type="password"
         placeholder="Password..."
