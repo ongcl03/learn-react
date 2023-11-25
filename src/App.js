c5cf15c4415b558be86d2b416fc3b1731e3fa82b
CL <ongchangle03@gmail.com>
2023-11-26 01:26:41 +0800

    Remove code from previous couse

--- src/App.js
+++ src/App.js
@@ -1,36 +1,7 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import { Home } from "./pages/Home";
-import { Profile } from "./pages/Profile";
-import { Contact } from "./pages/Contact";
-import { Navbar } from "./Navbar";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
-  const client = new QueryClient({
-    // set the configurations we want
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false,
-      },
-    },
-  });
-
-  return (
-    <div className="App">
-      <QueryClientProvider client={client}>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="*" element={<h1>Page not found</h1>} />
-          </Routes>
-        </Router>
-      </QueryClientProvider>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
--- src/Planet.js
+++
@@ -1,7 +0,0 @@
-export const Planet = (props) => {
-  return (
-    <div>
-      {props.isGasPlanet && props.name}
-    </div>
-  );
-}
