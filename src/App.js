0b099653117a57fd73bbf7ef24d820bdd0224384
CL <ongchangle03@gmail.com>
2023-11-26 02:57:14 +0800

    Remove code from previous course

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import "./App.css";
-import { Form } from "./components/Form";
 
 function App() {
-  return (
-    <div className="App">
-      <Form />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
