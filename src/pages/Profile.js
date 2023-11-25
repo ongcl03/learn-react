3c4ad6fd003df84a77a8634541e990e7fc0ba949
CL <ongchangle03@gmail.com>
2023-11-26 01:18:08 +0800

    Remove code from previous couse

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,15 +1,3 @@
-import { ChangeProfile } from "../components/ChangeProfile";
-import { useContext } from "react";
-import { AppContext } from "../App";
-
 export const Profile = () => {
-  // Using useContext here and get the value that is stored in AppContext
-  const { username, setUsername } = useContext(AppContext);
-
-  return (
-    <div>
-      Profile Page, user is {username}
-      <ChangeProfile />
-    </div>
-  );
+  return <h1>Profile Page</h1>;
 };
