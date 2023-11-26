5186e27b8766b8385e73c676c63326a1f9e66c60
CL <ongchangle03@gmail.com>
2023-11-26 15:22:28 +0800

    Create a custom hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import { useToggle } from "./useToggle";
 
 function App() {
-  return <div className="App"></div>;
+  // Using custom hook (they are independent to each other)
+  const [isVisible, toogle] = useToggle();
+  const [isVisible2, toogle2] = useToggle();
+
+  return (
+    <div className="App">
+      <button onClick={toogle}>{isVisible ? "Hide" : "Show"}</button>
+      {isVisible && <h1>Hidden text</h1>}
+      <button onClick={toogle}>{isVisible2 ? "Hide" : "Show"}</button>
+      {isVisible2 && <h1>Hidden text</h1>}
+    </div>
+  );
 }
 
 export default App;
